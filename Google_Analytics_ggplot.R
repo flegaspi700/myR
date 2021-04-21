2c8b41fcc6644403a8d0f191c7bf790a618a70c8
Frederick Legaspi <flegaspi700@yahoo.com>
2021-04-21 13:06:09 -0400

    Testing ggplot2 with Player_data

--- Google_Analytics_ggplot.R
+++ Google_Analytics_ggplot.R
@@ -1,3 +1,17 @@
 install.packages("ggplot2")
 install.packages("palmerpenguins")
 
+library(ggplot2)
+library(palmerpenguins)
+
+data("penguins")
+
+View(penguins)
+
+ggplot(data=penguins) + geom_point(mapping = aes(x=flipper_length_mm, y=body_mass_g))
+
+player_df <- read.csv("player_data.txt")
+View(player_df)
+
+ggplot(data=player_df) + geom_point(mapping = aes(x=team_pace, y=MPG))
+
