a4f93010a73141f19c7f1d22d9a175a0e52b1146
Frederick Legaspi <flegaspi700@yahoo.com>
2021-04-21 13:46:26 +0000

    More cleaning functions

--- Google_Analytics_cleaning.R
+++ Google_Analytics_cleaning.R
@@ -2,12 +2,14 @@ install.packages("here")
 install.packages("skimr")
 install.packages("janitor")
 install.packages("dplyr")
+install.packages("tidyr")
 install.packages("palmerpenguins")
 
 library("here")
 library("skimr")
 library("janitor")
 library("dplyr")
+library("tidyr")
 library("palmerpenguins")
 
 skim_without_charts(penguins)
@@ -27,3 +29,32 @@ rename_with(penguins, toupper)
 rename_with(penguins, tolower)
 
 clean_names(penguins)
+
+#Organizing your data
+penguins %>%
+  arrange(bill_length_mm)
+
+#descending order
+penguins %>%
+  arrange(-bill_length_mm)
+
+penguins2 <- penguins %>%
+  arrange(-bill_length_mm)
+
+View(penguins2)
+
+penguins2 %>% group_by(island) %>% 
+  drop_na() %>%
+  summarize(mean_bill_length_mm=mean(bill_length_mm))
+
+penguins2 %>% group_by(island) %>% 
+  drop_na() %>%
+  summarize(max_bill_length_mm=max(bill_length_mm))
+
+penguins2 %>% group_by(species, island) %>% 
+  drop_na() %>%
+  summarize(max_mm=max(bill_length_mm),mean_bl=mean(bill_length_mm))
+
+penguins2 %>%
+  filter(species=="Adelie")
+
